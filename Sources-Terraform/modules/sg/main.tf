b5d65ad6ad1be2048fecfcac5e37271297885a15
henrypoms <35367777+henrypoms@users.noreply.github.com>
2022-03-21 21:43:09 +0100

    Update main.tf

--- Sources-Terraform/modules/sg/main.tf
+++ Sources-Terraform/modules/sg/main.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "my_sg" {
+ressource "aws_security_group" "my_sg" {
   name        = "${var.maintainer}-sg"
   description = "Allow http, https  and ssh inbound traffic"
 
