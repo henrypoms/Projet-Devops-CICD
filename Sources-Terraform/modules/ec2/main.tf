529ed3c85b179c2d9a9f82581f16d0d33ab29d0e
henrypoms <35367777+henrypoms@users.noreply.github.com>
2022-03-22 16:36:01 +0100

    Update main.tf

--- Sources-Terraform/modules/ec2/main.tf
+++ Sources-Terraform/modules/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ic-webapp-ec2" {
   ami               = "ami-033b95fb8079dc481"
-  vpc_id            = data.aws_vpc.selected.id
+  vpc_id            = vpc-09d6da5e18e139a5d
   instance_type     = var.instance_type
   key_name          = var.ssh_key
   availability_zone = var.AZ
